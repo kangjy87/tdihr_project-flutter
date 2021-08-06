0581516b6d141185105d9ff0e7571fb606050aa7
lee-jae-hun <jaehun.lee@nsmg21.com>
2021-08-06 16:15:34 +0900

    webview useragent

--- lib/Page/TDIGroupwarePage.dart
+++ lib/Page/TDIGroupwarePage.dart
@@ -37,7 +37,8 @@ class TDIGroupwarePageState extends State<TDIGroupwarePage> {
         child: WillPopScope(
           onWillPop: () => _goBack(context),
           child: WebView(
-            userAgent: 'random',
+            userAgent:
+                'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36',
             initialUrl: URL.tdiLogin + TDIUser.token!.token,
             onWebViewCreated: (WebViewController webViewController) {
               _controllerComplete.complete(webViewController);
